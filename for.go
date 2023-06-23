6059d5c38c3307697a07a20fb69cc1ecde27c4e4
hangliuc <108672020+hangliuc@users.noreply.github.com>
2023-06-23 23:29:55 +0800

    Update for.go

--- for.go
+++ for.go
@@ -10,3 +10,21 @@ func main() {
 		fmt.Println()
 	}
 }
+
+// 函数调用
+func printMuIti(num int) {
+	for i := 1; i <= num; i++ { // 打印层
+		for j := 1; j <= i; j++ { //打印列数
+			fmt.Printf("%v * %v = %v ", j, i, j*i)
+		}
+		fmt.Println()
+	}
+
+}
+
+func main() {
+	var num int
+	fmt.Println("请输入乘法表对应数")
+	fmt.Scanln(&num)
+	printMuIti(num)
+}
