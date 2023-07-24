b5c37ec54eb546a225717f4cf1b3a5889e769b8d
Hang <iliuhang@163.com>
2023-07-24 18:40:09 +0800

    demo3

--- slice/demo3.go
+++ slice/demo3.go
@@ -1,1 +1,18 @@
 package main
+
+import "fmt"
+
+func main() {
+	var arr [5]int = [...]int{10, 20, 30, 40, 50}
+	slice := arr[1:4]
+	fmt.Println(len(slice))
+	for i := 0; i < len(slice); i++ {
+		fmt.Printf("slice[%v]=%v\n", i, slice[i])
+	}
+
+	fmt.Println()
+
+	for i, v := range slice { // i数组的下标，数组的值
+		fmt.Printf("i=%v v=%v\n", i, v)
+	}
+}
