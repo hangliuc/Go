440a3fa134339214c628563e7641a665315088de
Hang <iliuhang@163.com>
2023-07-31 17:22:15 +0800

    sort

---
+++ Finding/demo2.go
@@ -0,0 +1,12 @@
+// https://www.kancloud.cn/imdszxs/golang/1509548
+package main
+
+import "fmt"
+
+func BinaryFind(arr *[6]int, leftIndex int, rightIndex int, findVal int) {
+
+}
+func main() {
+
+	fmt.Println()
+}
